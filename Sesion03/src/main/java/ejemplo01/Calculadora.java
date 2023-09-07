75641a3dffb7bcc5fdd4e99e4ed4bc0d7f08563d
Dyeseniact <camposdyes@gmail.com>
2023-09-06 20:08:31 -0600

    Reto 01: Prueba de Exceptions

--- Sesion03/src/main/java/ejemplo01/Calculadora.java
+++ Sesion03/src/main/java/ejemplo01/Calculadora.java
@@ -1,3 +1,5 @@
+package ejemplo01;
+
 public class Calculadora {
 
     public int suma(int a, int b) {
@@ -10,4 +12,12 @@ public class Calculadora {
     public int multiplicacion(int a, int b) {
         return a * b;
     }
+
+    public float divide(float a, float b) throws IllegalAccessException {
+        if(b == 0) {
+            throw new IllegalAccessException("No es posible dividir un valor entre 0");
+        }
+        return a / b;
+    }
+
 }
--- Sesion03/src/test/java/ejemplo01/CalculadoraTest.java
+++ Sesion03/src/test/java/ejemplo01/CalculadoraTest.java
@@ -1,4 +1,51 @@
+package ejemplo01;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculadoraTest {
-  
+    private Calculadora calculadora = new Calculadora();
+    @Test
+    @DisplayName("Prueba suma")
+    void suma() {
+        int esperado = 5;
+
+        assertEquals(esperado, calculadora.suma(3,2));
+
+    }
+
+    @Test
+    @DisplayName("Prueba resta")
+    void resta() {
+        int esperado = 1;
+
+        assertEquals(esperado, calculadora.resta(3,2));
+    }
+
+    @Test
+    @DisplayName("Prueba multiplicacion")
+    void multiplicacion() {
+        int esperado = 6;
+
+        assertEquals(esperado, calculadora.multiplicacion(3,2));
+    }
+
+    @Test
+    @DisplayName("Prueba Division Error")
+    void divicionErrorTest() {
+        Throwable exception = assertThrows(IllegalAccessException.class, () -> calculadora.divide(5,0));
+
+        assertEquals("No es posible dividir un valor entre 0", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Prueba Division")
+    void divicionTest() throws IllegalAccessException {
+        int esperado = 2;
+        assertEquals(esperado, calculadora.divide(10.0f,5.0f));
+    }
+
+
 }
