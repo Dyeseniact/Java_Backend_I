39a12fcbf078050c2c574e1aa7fd544f4fd9a1ad
Dyeseniact <camposdyes@gmail.com>
2023-09-11 21:11:35 -0600

    Reto 01: Controladores con Spring MVC con parámetros

--- Sesion04/s4/src/main/java/org/bedu/java/backend/s4/controllers/SaludoController.java
+++ Sesion04/s4/src/main/java/org/bedu/java/backend/s4/controllers/SaludoController.java
@@ -2,15 +2,17 @@ package org.bedu.java.backend.s4.controllers;
 
 import org.bedu.java.backend.s4.models.Saludo;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SaludoController {
 
-    @GetMapping("/saludo")
-    public Saludo saluda(){
+    @GetMapping("/saludo/{nombre}") //lectura
+    public Saludo saluda(@PathVariable String nombre){
         Saludo saludo = new Saludo();
         saludo.setMensaje("Hola Mundo!!!");
+        saludo.setNombre(nombre);
 
         return saludo;
     }
--- Sesion04/s4/src/main/java/org/bedu/java/backend/s4/models/Saludo.java
+++ Sesion04/s4/src/main/java/org/bedu/java/backend/s4/models/Saludo.java
@@ -3,6 +3,16 @@ package org.bedu.java.backend.s4.models;
 public class Saludo {
     private String mensaje;
 
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public String getMensaje() {
         return mensaje;
     }
