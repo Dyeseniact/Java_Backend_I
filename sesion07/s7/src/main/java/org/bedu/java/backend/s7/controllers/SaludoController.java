0d3bf903dafd11c380d6a7760f2893a3ec141c0b
Dyeseniact <camposdyes@gmail.com>
2023-09-20 20:57:32 -0600

    Reto 01: Valores dinámicos con Thymeleaf y Ejemplo 02: Manejo de formularios

--- sesion07/s7/src/main/java/org/bedu/java/backend/s7/controllers/SaludoController.java
+++ sesion07/s7/src/main/java/org/bedu/java/backend/s7/controllers/SaludoController.java
@@ -2,15 +2,16 @@ package org.bedu.java.backend.s7.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SaludoController {
 
     @GetMapping("/hola")
-    public ModelAndView hola(){
+    public ModelAndView hola(@RequestParam(name = "mensaje") String mensaje){
         ModelAndView modelAndView = new ModelAndView("hola");
-        modelAndView.addObject("mensaje", "Hola desde thymeleaf");
+        modelAndView.addObject("mensaje", mensaje);
         return modelAndView;
     }
 
